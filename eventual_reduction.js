845c2dd4a470a32b9878733e285233325b29b41c
Anugeeth <anugeeth2@gmail.com>
2020-07-20 10:33:37 +0530

    ,' ;

--- eventual_reduction.js
+++ eventual_reduction.js
@@ -1,28 +1,27 @@
 const readline = require('readline');
 const process = require('process');
-(function(){
-    var test_cases;
+(function () {
     const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-     rl.question(`Test Cases?\t`, (value) => test_cases = Number(value));
-    while (test_cases--) {
-        var n, s;
-        console.log('here')
-        rl.question(`Enter n?\t`, (value) => {
-            n = Number(value);
-            rl.question(`TEnter string?\t`, (data) => s = data);
-        });    
-        for (let j = 'a'; j <= 'z'; j++) {
-            let c = 0;
-            for (k = 0; k < n; k++) {
-                if (j == s[k]) c++;
-            }
-            if (c % 2 == 0) d = 0;
-            else {
-                d = 1;
-                break;
-            }
+    rl.question(`Test Cases?\t`, (test_cases) => {
+        while (test_cases--) {
+            rl.question(`Enter n?\t`, (value) => {
+               let n = Number(value);
+                rl.question(`TEnter string?\t`, (s) => {
+                    for (let j = 'a'; j <= 'z'; j++) {
+                        let c = 0;
+                        for (k = 0; k < n; k++) {
+                            if (j == s[k]) c++;
+                        }
+                        if (c % 2 == 0) d = 0;
+                        else {
+                            d = 1;
+                            break;
+                        }
+                    }
+                    if (d == 0) console.log('YES')
+                    else console.log('NO');
+                });
+            });
         }
-        if (d == 0) console.log('YES')
-        else console.log('NO');
-    }
+    });
 })();
