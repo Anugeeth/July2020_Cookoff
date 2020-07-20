03af9eafd38d79f6da2b298b67e1aead7b29606b
Sarang N <sarang98divakaran@gmail.com>
2020-07-20 20:42:50 +0530

    Update eventual_reduction.js

--- eventual_reduction.js
+++ eventual_reduction.js
@@ -1,27 +1,39 @@
 const readline = require('readline');
-const process = require('process');
-(function () {
-    const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-    rl.question(`Test Cases?\t`, (test_cases) => {
-        while (test_cases--) {
-            rl.question(`Enter n?\t`, (value) => {
-               let n = Number(value);
-                rl.question(`TEnter string?\t`, (s) => {
-                    for (let j = 'a'; j <= 'z'; j++) {
-                        let c = 0;
-                        for (k = 0; k < n; k++) {
-                            if (j == s[k]) c++;
-                        }
-                        if (c % 2 == 0) d = 0;
-                        else {
-                            d = 1;
-                            break;
-                        }
-                    }
-                    if (d == 0) console.log('YES')
-                    else console.log('NO');
-                });
-            });
-        }
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+// Promisify Readline.Question
+async function ask(question) {
+    return new Promise((resolve, reject) => {
+        rl.question(question, resolve);
     });
-})();
+}
+
+// Enclose in a function to use async await
+async function testCases() {
+    let cases = await ask('Test cases: ');
+    while (cases--) {
+        let n = await ask('N: ');
+        n = Number(n);
+
+        let s = await ask('String: ');
+
+        for (let j = 'a'; j <= 'z'; j++) {
+            let c = 0;
+            for (k = 0; k < n; k++) {
+                if (j == s[k]) c++;
+            }
+            if (c % 2 == 0) d = 0;
+            else {
+                d = 1;
+                break;
+            }
+        }
+        if (d == 0) console.log('YES');
+        else console.log('NO');
+    }
+    // close rl to exit program
+    rl.close();
+}
+
+testCases();
